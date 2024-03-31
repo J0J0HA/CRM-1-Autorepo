8d912fdae40b010b450c632179799a3a2b599e21
Restyled.io <commits@restyled.io>
2024-03-31 18:31:59 +0000

    Restyled by black

--- utils/provider/__init__.py
+++ utils/provider/__init__.py
@@ -10,14 +10,15 @@ class _ProviderTyping:
         # This is only a type hint. look in the forgejo.py and github.py for the actual implementation
         raise NotImplementedError
 
-    async def get_releases(self, settings: datacls.ModSettings,
-                           repo: datacls.Repo) -> list[datacls.Release]:
+    async def get_releases(
+        self, settings: datacls.ModSettings, repo: datacls.Repo
+    ) -> list[datacls.Release]:
         # This is only a type hint. look in the forgejo.py and github.py for the actual implementation
         raise NotImplementedError
 
     async def get_latest_commit_as_release(
-            self, settings: datacls.ModSettings,
-            repo: datacls.Repo) -> datacls.Release:
+        self, settings: datacls.ModSettings, repo: datacls.Repo
+    ) -> datacls.Release:
         # This is only a type hint. look in the forgejo.py and github.py for the actual implementation
         raise NotImplementedError
 
