4720cbca49af9ee1040530bd2cc25251a9870ea4
Restyled.io <commits@restyled.io>
2024-03-31 18:29:24 +0000

    Restyled by yapf

--- utils/provider/__init__.py
+++ utils/provider/__init__.py
@@ -5,19 +5,19 @@ from .. import data as datacls
 
 class _ProviderTyping:
     """ """
+
     async def get_repo(self, settings: datacls.ModSettings) -> datacls.Repo:
         # This is only a type hint. look in the forgejo.py and github.py for the actual implementation
         raise NotImplementedError
 
-    async def get_releases(
-        self, settings: datacls.ModSettings, repo: datacls.Repo
-    ) -> list[datacls.Release]:
+    async def get_releases(self, settings: datacls.ModSettings,
+                           repo: datacls.Repo) -> list[datacls.Release]:
         # This is only a type hint. look in the forgejo.py and github.py for the actual implementation
         raise NotImplementedError
 
     async def get_latest_commit_as_release(
-        self, settings: datacls.ModSettings, repo: datacls.Repo
-    ) -> datacls.Release:
+            self, settings: datacls.ModSettings,
+            repo: datacls.Repo) -> datacls.Release:
         # This is only a type hint. look in the forgejo.py and github.py for the actual implementation
         raise NotImplementedError
 
