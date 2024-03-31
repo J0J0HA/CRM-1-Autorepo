c0e08be2b3940875c52e203d01e687a1a79aa4dd
Restyled.io <commits@restyled.io>
2024-03-31 18:31:38 +0000

    Restyled by black

--- utils/provider/forgejo.py
+++ utils/provider/forgejo.py
@@ -6,16 +6,19 @@ import requests
 from .. import datacls
 
 
-async def get_repo(session: aiohttp.ClientSession,
-                   settings: datacls.ModSettings) -> datacls.Repo:
+async def get_repo(
+    session: aiohttp.ClientSession, settings: datacls.ModSettings
+) -> datacls.Repo:
     instance = (settings.instance or "https://codeberg.org").removesuffix("/")
     async with session.get(f"{instance}/api/v1/repos/{settings.repo}") as resp:
         repo_data = await resp.json()
-    async with session.get(f"{instance}/api/v1/repos/{settings.repo}/commits",
-                           timeout=10) as commits_resp:
+    async with session.get(
+        f"{instance}/api/v1/repos/{settings.repo}/commits", timeout=10
+    ) as commits_resp:
         commits_data = await commits_resp.json()
     contributors = list(
-        set(commit["commit"]["author"]["name"] for commit in commits_data))
+        set(commit["commit"]["author"]["name"] for commit in commits_data)
+    )
     return datacls.Repo(
         name=repo_data["full_name"],
         git_url=repo_data["clone_url"],
@@ -27,11 +30,11 @@ async def get_repo(session: aiohttp.ClientSession,
     )
 
 
-async def get_releases(session: aiohttp.ClientSession,
-                       settings: datacls.ModSettings, repo: datacls.Repo):
+async def get_releases(
+    session: aiohttp.ClientSession, settings: datacls.ModSettings, repo: datacls.Repo
+):
     instance = (settings.instance or "https://codeberg.org").removesuffix("/")
-    async with session.get(
-            f"{instance}/api/v1/repos/{repo.name}/releases") as resp:
+    async with session.get(f"{instance}/api/v1/repos/{repo.name}/releases") as resp:
         releases_data = await resp.json()
     return [
         datacls.Release(
@@ -39,20 +42,22 @@ async def get_releases(session: aiohttp.ClientSession,
             version=r["tag_name"].removeprefix("v").removeprefix("V"),
             title=r["name"],
             body=r["body"],
-            attached_files=[(a["name"], a["browser_download_url"])
-                            for a in r["assets"]],
+            attached_files=[
+                (a["name"], a["browser_download_url"]) for a in r["assets"]
+            ],
             by=r["author"]["login"],
-            published_at=datetime.strptime(r["published_at"].replace(":", ""),
-                                           "%Y-%m-%dT%H%M%S%z").timestamp(),
+            published_at=datetime.strptime(
+                r["published_at"].replace(":", ""), "%Y-%m-%dT%H%M%S%z"
+            ).timestamp(),
             prerelease=r["prerelease"],
             link=r["html_url"],
             is_prebuilt=True,
-        ) for r in releases_data
+        )
+        for r in releases_data
     ]
 
 
-def get_latest_commit_as_release(settings: datacls.ModSettings,
-                                 repo: datacls.Repo):
+def get_latest_commit_as_release(settings: datacls.ModSettings, repo: datacls.Repo):
     """
 
     :param settings: datacls.ModSettings:
@@ -70,8 +75,9 @@ def get_latest_commit_as_release(settings: datacls.ModSettings,
         body=commit["commit"]["message"],
         attached_files=[],
         by=commit["commit"]["author"]["name"],
-        published_at=datetime.strptime(commit["created"].replace(":", ""),
-                                       "%Y-%m-%dT%H%M%S%z").timestamp(),
+        published_at=datetime.strptime(
+            commit["created"].replace(":", ""), "%Y-%m-%dT%H%M%S%z"
+        ).timestamp(),
         prerelease=True,
         link=commit["html_url"],
         is_prebuilt=False,
